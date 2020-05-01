86b5fdaf9198853b488aea86ffc02ea1e955c56d
Dimeji <afasina@nasdng.com>
2020-05-01 17:48:20 +0100

    paystack payment

--- src/core/Paystack.js
+++ src/core/Paystack.js
@@ -1,77 +1,89 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, {Component ,  Fragment, useEffect, useState } from "react";
 import Header from "./Header";
 import Aside from "./Aside";
-import { Link, Redirect } from "react-router-dom";
 import { isAuthenticated } from "../auth";
-import ShowImage from './ShowImage';
-import randomstring from "randomstring";
-import { getPaymentByRef } from "./ApiCore";
-import PaystackButton from "react-paystack";
+import { getPaymentByRef, getBraintreeClientToken, processPayment } from "./ApiCore";
+import Dropln from "braintree-web-drop-in-react";
+import PaystackButton from 'react-paystack';
 
-const Paystack = ({match}) => {
-    const { user, token } = isAuthenticated();
+const Paystack = ({ match }) => {
+  const { user, token } = isAuthenticated();
 
-      const [refre, setrefre] = useState([])
-      const [error, setError] = useState(false)
+  const [refre, setrefre] = useState([]);
 
-     
+  const [values, setValues] = useState({
+    clientToken: null,
+    instance: {},
+    error: false
+  });
+
+  const callback = (response) => {
+    console.log(response); // card charged successfully, get reference here
+  }
+
+  const close = () => {
+    console.log("Payment closed");
+  }
 
 
-      const init = refId => {
-        getPaymentByRef(refId, user._id, token).then(data => {
-          if (data.error) {
-           setError(data.error)
-          } else {
-            setrefre(data);
-          }
-        });
-      };
 
-let email, amount
-      const process = () => {
-          refre.map((r,i) => {
-            email = r.userId.email
-            amount = r.amount
-          })
+  const init = refId => {
+    getPaymentByRef(refId, user._id, token).then(data => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setrefre(data);
       }
+    });
+  };
 
-      
-    
-    
-      
+  let email, amount
+  let key ='pk_test_a3c6eed2d7700ebb41bf5417adeee9ae037f0fdc'
+        const process = () => {
+            refre.map((r,i) => {
+              email = r.userId.email
+              amount = r.amount
+            })
+        }
 
-    
-      useEffect(() => {
-        init(match.params.refId);
-      }, []);
+  useEffect(() => {
+    init(match.params.refId);
 
-      const [key, setKey] = useState(
-        "pk_test_a3c6eed2d7700ebb41bf5417adeee9ae037f0fdc"
-      );
-   
-    
-      const callback =  response => {
-        console.log(response); // card charged successfully, get reference here
-      };
-      const close = () => {
-        console.log("Payment closed");
-      };
+  }, []);
+
+  
+  const showError = (error) => (
+    <div style={{display: error ? '' : 'none'}} class="alert alert-danger alert-dismissible bg-danger text-white border-0 fade show" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+    <strong>Error - </strong> {error}
+</div>
+  );
 
 
+  const content = () => {
+    return (
+      <Fragment>
+        <div className="row">
+          <div className="col-md-8">
+            <div className="card box-margin">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-12">
+                    <div className="card-body">
+                      <div className="checkout-area mb-50">
+                 
+                        <h4 className="card-title mt-0 mb-3">Fund Project {process()} (₦{amount})</h4>
+                        {showError(values.error)}
+                        
+                          <div className="row">
+                          
+                          </div>
 
-
-      return (
-        <Fragment>
-          <div className="ecaps-page-wrapper">
-            <Aside></Aside>
-            <div className="ecaps-page-content">
-              <Header></Header>
-              <div className="main-content">
-                <div class="container-fluid">
-                <div>
-            <p>
-                {process()}
-                <PaystackButton
+                          <div className="mt-30">
+                          
+                            <PaystackButton
                 text="Make Payment"
                 class="payButton"
                 callback={callback()}
@@ -84,15 +96,37 @@ let email, amount
                 paystackkey={key}
                 tag="button"
               />
-            </p>
-          </div>
+                            </div>
+                       
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </Fragment>
-      );
+        </div>
+      </Fragment>
+    );
+  };
 
-    }
+  return (
+    <Fragment>
+      <div className="ecaps-page-wrapper">
+        <Aside></Aside>
+        <div className="ecaps-page-content">
+          <Header></Header>
+          <div className="main-content">
+            <div className="container-fluid">
+              <div className="mt-30">
+             
+                {content()}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
-    export default Paystack
+export default Paystack;
