c08bd637790339521f24970a55fa1b55e785f484
Dimeji <afasina@nasdng.com>
2020-05-04 10:03:34 +0100

    set up for presentation

--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -7,7 +7,7 @@ const AdminRoute = ({component: Component, ...rest}) => (
     <Route {...rest} render={props => isAuthenticated() && isAuthenticated().user.role === 1  ? (
         <Component {...props} />
     ) : (
-        <Redirect to={{pathname: '/signin', state:{from: props.location}}} />
+        <Redirect to={{pathname: '/', state:{from: props.location}}} />
     )} />
 );
 
--- src/auth/PrivateRoute.js
+++ src/auth/PrivateRoute.js
@@ -11,7 +11,7 @@ const PrivateRoute = ({component: Component, ...rest}) => (
         <Component {...props} />
     ) : (
         
-        <Redirect to={{pathname: '/signin', state:{from: props.location}}} />
+        <Redirect to={{pathname: '/', state:{from: props.location}}} />
     )} />
 );
 
